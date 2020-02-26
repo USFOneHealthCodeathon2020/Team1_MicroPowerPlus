0e53d70a66caf0a4556cd31763680a6e87abad00
DokurOmkar <DokurOmkar@users.noreply.github.com>
2020-02-26 15:41:29 -0500

    Update app.R

--- app.R
+++ app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(ggplot2)
+library(plotly)
 
 ui <- fluidPage(
   h1("Team 1"),
@@ -27,36 +28,53 @@ ui <- fluidPage(
      
     ),
     mainPanel(
-      plotOutput("plot1"),
-      plotOutput("plot2"),
-      
+      plotlyOutput("plot1"),
+      plotlyOutput("plot2")
     )
   )
 )
 
 server <- function(input, output, session) {
   
-  output$plot1 <- renderPlot({
-    effectsize <- matrix(rexp(input$power, rate=.1))
-    power <- matrix( c(1,2,3,4))
-    barplot(as.integer(effectsize, power),
-            main = "Power (%Probability you can detect diff)",
-            ylab="power",
-            xlab="microbes",
-            col = "blue")
+  # output$plot1 <- renderPlot({
+  #   effectsize <- matrix(rexp(input$power, rate=.1))
+  #   power <- matrix( c(1,2,3,4))
+  #   barplot(as.integer(effectsize, power),
+  #           main = "Power (%Probability you can detect diff)",
+  #           ylab="power",
+  #           xlab="microbes",
+  #           col = "blue")
+  # 
+  # })
   
-  })
+  output$plot1 <- renderPlotly(
+    plot3 <- plot_ly(
+      x = c(matrix(rexp(input$power, rate=.1))),
+      y = c(matrix(rexp(input$power, rate=.1))),
+      name = "Power (%Probability you can detect diff)",
+      type = "bar"
+    ) %>% layout(title="Power (%Probability you can detect diff)", yaxis=list(title="power"), xaxis=list(title="microbes"))
+  )
+ 
+  # output$plot2 <- renderPlot({
+  #   effectsize <- matrix(rexp(input$eSize, rate=.1))
+  #   power <- matrix( c(1,2,3,4))
+  #   barplot(as.integer(effectsize, power),
+  #           ylab="effect size",
+  #           xlab="microbes",
+  #           main="Effect Size (How big would diff have to be?)",
+  #           col = "red")
+  #   
+  # })
   
-  output$plot2 <- renderPlot({
-    effectsize <- matrix(rexp(input$eSize, rate=.1))
-    power <- matrix( c(1,2,3,4))
-    barplot(as.integer(effectsize, power),
-            ylab="effect size",
-            xlab="microbes",
-            main="Effect Size (How big would diff have to be?)",
-            col = "red")
-    
-  })
+  output$plot2 <- renderPlotly(
+    plot3 <- plot_ly(
+      x = c(matrix(rexp(input$eSize, rate=.1))),
+      y = c(matrix(rexp(input$eSize, rate=.1))),
+      name = "Effect Size (How big would diff have to be?)",
+      type = "bar"
+    ) %>% layout(title="Effect Size (How big would diff have to be?)", yaxis=list(title="effect size"), xaxis=list(title="microbes"))
+  )
 
 }
 
